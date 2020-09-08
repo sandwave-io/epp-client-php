f888f3513e0ac75142cc935ea1f3227d52a4d3d7
Jesse Kramer <jesse@kramerventures.nl>
2020-09-08 08:31:40 +0200

    Add requests

---
+++ src/Epp/Rfc/Requests/Contact/ContactCheckRequest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+
+namespace SandwaveIo\EppClient\Epp\Rfc\Requests\Contact;
+
+use DOMElement;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\ClientTransactionIdentifier;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\Command;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\Commands\Check;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\Contact\ContactCheck;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\Contact\ContactId;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\Domain\DomainCheck;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\Domain\DomainName;
+use SandwaveIo\EppClient\Epp\Rfc\Elements\Epp;
+use SandwaveIo\EppClient\Epp\Rfc\Requests\Request;
+
+class ContactCheckRequest extends Request
+{
+    /** @var array<string> */
+    private $contacts;
+
+    public function __construct(array $contacts)
+    {
+        parent::__construct();
+
+        $this->contacts = $contacts;
+    }
+
+    protected function renderElements(): DOMElement
+    {
+        return Epp::render([
+            Command::render([
+                Check::render([
+                    ContactCheck::render(
+                        array_map(function (string $domain) {
+                            return ContactId::render([], $domain);
+                        }, $this->contacts)
+                    ),
+                ]),
+                $this->clientTransactionIdentifier ? ClientTransactionIdentifier::render([], $this->clientTransactionIdentifier) : null,
+            ]),
+        ], null, $this->extensions);
+    }
+}
