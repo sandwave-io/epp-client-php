f1d7936a614a95fdaf55a64a9338a680ca8a7be2
Thomas Ploegaert <thomas@sandwave.io>
2020-09-11 09:23:41 +0200

    forgotten a part of the domain info tests

--- tests/Responses/Extensions/Sidn/SidnDomainInfoResponseTest.php
+++ tests/Responses/Extensions/Sidn/SidnDomainInfoResponseTest.php
@@ -2,6 +2,7 @@
 
 namespace SandwaveIo\EppClient\Tests\Responses\Extensions\Sidn;
 
+use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
 use SandwaveIo\EppClient\Epp\Extensions\Responses\Sidn\SidnDomainInfoResponse;
 use SandwaveIo\EppClient\Epp\Rfc\Document;
@@ -19,6 +20,37 @@ class SidnDomainInfoResponseTest extends TestCase
         $this->assertSame('ABC-12345', $response->getClientTransactionIdentifier(), 'Failed assertion on clTRID text.');
         $this->assertSame('54322-XYZ', $response->getServerTransactionIdentifier(), 'Failed assertion on svTRID text.');
 
+        $this->assertSame('example.com', $response->getDomainName());
+        $this->assertSame('EXAMPLE1-REP', $response->getRepositoryObjectIdentifier());
+        $this->assertSame(['ok'], $response->getStatuses());
+
+        $this->assertSame(['jd1234'], $response->getRegistrants());
+        $this->assertSame([
+            'admin' => 'sh8013',
+            'tech' => 'sh8013',
+        ], $response->getContacts());
+
+        $this->assertSame([
+            'ns1.example.com',
+            'ns1.example.net',
+        ], $response->getNameServers());
+        $this->assertSame([
+            'ns1.example.com',
+            'ns2.example.com',
+        ], $response->getHosts());
+
+        $this->assertSame('ClientX', $response->getClientId());
+        $this->assertSame('ClientY', $response->getCreationClientId());
+        $this->assertSame('ClientX', $response->getLastUpdateClientId());
+
+        $this->assertInstanceOf(Carbon::class, $response->getCreatedDate());
+        $this->assertInstanceOf(Carbon::class, $response->getUpdatedDate());
+        $this->assertInstanceOf(Carbon::class, $response->getExpirationDate());
+        $this->assertInstanceOf(Carbon::class, $response->getTransferedDate());
+
+        $this->assertSame('2fooBAR', $response->getPassword());
+
+        //sidn
         $this->assertSame('example.com', $response->getDomainName(), 'Failed assertion on domain name');
         $this->assertSame('1', $response->getDomainPeriod(), 'Failed assertion on domain period');
         $this->assertSame('??', $response->getDomainOptout(), 'Failed assertion on domain optout');
